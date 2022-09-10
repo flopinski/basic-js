e01afeb1b1a101a6971560922f8f968d5704c586
flopinski <103384762+flopinski@users.noreply.github.com>
2022-09-10 12:38:29 +0200

    feat: done with Count Cats!

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(array) {
-  
+function countCats(matrix) {
+  let count = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "^^") count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
